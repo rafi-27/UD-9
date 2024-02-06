a51417bffe796d94e95475e87a753169d1010d1a
rafik <rafikbachri21@gmail.com>
2024-02-06 13:22:14 +0100

    06/

--- src/pruebas.java
+++ src/pruebas.java
@@ -5,9 +5,15 @@ public class pruebas {
     coche cocheUno = new coche("EOI232", vehiculo.Color.NEGRO,120);
     moto motouno = new moto("Kawasaki", 2, vehiculo.Color.BLANCO, false);
     coche porshe = new coche("TH3940", vehiculo.Color.ROJO, 750);
+    vehiculo vehicle = new vehiculo("Si", 15, vehiculo.Color.AMARILLO);
+
     System.out.println(cocheUno);
     System.out.println(motouno);
     System.out.println();
     System.out.println(porshe);
-    }
+    
+    System.out.println();
+    System.out.println(vehicle);
+
+}
 }
