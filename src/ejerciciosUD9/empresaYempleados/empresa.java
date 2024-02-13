433dbf37e8a566c95598a8b811ef66a4a3a8bede
rafik <rafikbachri21@gmail.com>
2024-02-13 13:20:14 +0100

    final

--- src/ejerciciosUD9/empresaYempleados/empresa.java
+++ src/ejerciciosUD9/empresaYempleados/empresa.java
@@ -1,17 +1,31 @@
 package ejerciciosUD9.empresaYempleados;
 
+import java.util.ArrayList;
+
 public class empresa {
     private final String nombreEmpresa;
     private final String CIF;
+    private String telefono;
+    private String direccionEmpresa;
+    ArrayList<empleados>listaEmpleados = new ArrayList<>();
     /**
-     * Constructor:
+     * Constructor de empresa.
      * @param nombreEmpresa
      * @param cIF
+     * @param telefono
+     * @param direccionEmpresa
+     * @param listaEmpleados
      */
-    public empresa(String nombreEmpresa, String cIF) {
+    public empresa(String nombreEmpresa, String cIF, String telefono, String direccionEmpresa,
+            ArrayList<empleados> listaEmpleados) {
         this.nombreEmpresa = nombreEmpresa;
         CIF = cIF;
+        this.telefono = telefono;
+        this.direccionEmpresa = direccionEmpresa;
+        this.listaEmpleados = listaEmpleados;
     }
+    
+    
 
     
 
