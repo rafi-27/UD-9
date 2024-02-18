2c1bfadce0e925b90a3b6c9e22b99695fc66bb54
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-02-18 19:32:10 +0100

    si

--- src/maraton/figuraMaraton.java
+++ src/maraton/figuraMaraton.java
@@ -4,10 +4,8 @@ public class figuraMaraton {
     public static void main(String[] args) {
         Triangulo uno = new Triangulo(5, 2.5, "Equilatero");
 
-        //System.out.println(uno.area());
+        System.out.println(uno.area());
         uno.verDim();
         System.out.println(uno.verEstilo());
-
-        
     }
 }
