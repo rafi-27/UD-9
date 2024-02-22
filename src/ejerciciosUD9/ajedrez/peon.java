a556b19c99ebb0675e48ba9a2bc35967eba3be42
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-02-22 19:39:44 +0100

    19:39

--- src/ejerciciosUD9/ajedrez/peon.java
+++ src/ejerciciosUD9/ajedrez/peon.java
@@ -2,19 +2,35 @@ package ejerciciosUD9.ajedrez;
 
 import java.awt.Point;
 
-public class peon extends pieza{
+public class peon extends pieza {
     /**
-    * Constructor similar al de la clase torre:
-    */
-    public peon(int x,int y, ColorPieza color){
+     * Constructor similar al de la clase torre:
+     */
+    public peon(int x, int y, ColorPieza color) {
         super(x, y, color);
     }
 
     /**
-    * Metodo para mover la pieza:
-    */
+     * Metodo para mover la pieza:
+     */
     @Override
     void mover(int x, int y) {
-        
+        if (this.getColorpieza() == ColorPieza.BLANCO) {
+            if (this.getPosicion().x == 1 && this.getPosicion().y == y) {
+                if (x == 1 || x == 2) {
+                    setPosicion(new Point(this.getPosicion().x + x, y));
+                }
+            } else if (this.getPosicion().y == y) {
+                setPosicion(new Point(this.getPosicion().x + 1, y));
+            }
+        }else{
+            if (this.getPosicion().x == 8 && this.getPosicion().y == y) {
+                if (x == 1 || x == 2) {
+                    setPosicion(new Point(this.getPosicion().x - x, y));
+                }
+            }else if (this.getPosicion().y == y) {
+                setPosicion(new Point(this.getPosicion().x-1, y));
+            }
+        }
     }
 }
--- src/ejerciciosUD9/ajedrez/torre.java
+++ src/ejerciciosUD9/ajedrez/torre.java
@@ -2,20 +2,62 @@ package ejerciciosUD9.ajedrez;
 
 import java.awt.Point;
 
-public class torre extends pieza {
+public class torre extends pieza implements JuegoTablero {
     /**
-    * Implementamos el primer constructor de nuesta clase pieza.
-    */
-    public torre(int x, int y,ColorPieza colorPieza){
+     * Implementamos el primer constructor de nuesta clase pieza.
+     */
+    public torre(int x, int y, ColorPieza colorPieza) {
         super(x, y, colorPieza);
     }
 
+    /**
+     * Moveremos las fichas mientras una coordenada se mantenga igual.
+     */
     @Override
     void mover(int x, int y) {
-        if (comprobarPosicion(x)&&comprobarPosicion(y)) {
-            if (this.getPosicion().x==x || this.getPosicion().y==y) {
+        if (comprobarPosicion(x) && comprobarPosicion(y)) {
+            if (this.getPosicion().x == x || this.getPosicion().y == y) {
                 setPosicion(new Point(x, y));
             }
         }
+    }
+
+    /**
+     * Procedemos a implementarle los interfaces:
+     */
+
+    // devolverá la letra correspondiente del tablero de ajedrez.
+    @Override
+    public char indiceAPosicion(int indice) {
+        String letraTablero = "ABCDEFGH";
+        return letraTablero.charAt(indice);
+    }
+
+    // devolverá su posición numérica
+    @Override
+    public int indiceAPosicionN(int indice) {
+        if (indice >= 0 && indice < 7) {
+            return indice + 1;
+        }
+        return -1;
+    }
+
+    // Devolvera posicion numerica de la letra.
+    @Override
+    public int posicionAIndice(char posicion) {
+        String letraTablero = "ABCDEFGH";
+        if (letraTablero.contains(String.valueOf(posicion))) {
+            return (letraTablero.indexOf(posicion) + 1);
+        }
+        return -1;
+    }
+
+    // devolverá su correspondiente índice numérico.
+    @Override
+    public int posicionAIndice(int posicion) {
+        if (posicion >= 0 && posicion <= 8) {
+            return posicion-1;
+        }
+        return -1;
     }
 }
